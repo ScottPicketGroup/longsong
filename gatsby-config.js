c7016029ce8862c497ee6d6484526cf72726d2ed
ScottPicketGroup <84173808+ScottPicketGroup@users.noreply.github.com>
2021-10-05 12:01:01 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,8 +40,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+         spaceId: process.env.CONTENTFUL_SPACE_ID,
+         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: `preview.contentful.com`,
       }
     },
   ],
