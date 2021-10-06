1ce3b1db41f02c762f27147f8c643bc73a729f81
marekreid <marekreid@gmail.com>
2021-10-06 23:48:06 +0200

    updated events with placeholder

---
+++ src/components/Pages/Landing/Caleneder/EventDetailsModal/EventDetailsModal.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import LongsongIcon from "../../../../MenuContainer/Icons/LongsongIcon"
+import {
+  EventDetailsWrapper,
+  EventListWrapper,
+  EventFooterWrapper,
+  EventInfoWrapper,
+  EventContents
+} from "../Calender.css"
+import { Heading3, Number1, Heading2 } from "../../../../global-styles/typography.css"
+import { DateDisplayContainer } from "../Calender.css"
+
+
+const EventDetailsModal = ({ open, setOpenModel, height, width, i, setOpen, day, dayOfWeek, todaysDate, item }) => {
+
+
+
+  return (
+
+    <EventDetailsWrapper
+      open={open}
+      height={height * 3}
+      width={width / 7 * 2 - 10}
+      i={i}
+      onClick={() => setOpen(false)}
+    >
+      <EventListWrapper width={width} height={height} i={i}>
+        <DateDisplayContainer
+          height={height}
+          width={width}
+          modal
+        >
+          <Heading3
+            calander
+            dayOfWeek={day} day={day} date={todaysDate} i={i} key={i + 1}>
+            {item.date == todaysDate + 1 ? 'Tomorrow' : item.day}
+          </Heading3>
+          <Number1 dayOfWeek={day} day={day} date={todaysDate} i={i} key={i + 2}>
+            {item.date}
+          </Number1>
+        </DateDisplayContainer>
+      </EventListWrapper>
+
+
+
+      <EventInfoWrapper>
+        <EventContents>
+        <Heading2>
+          Performer
+          </Heading2>
+          <Heading2>
+            The Huxleys
+            </Heading2>
+            <Heading2>
+            Happy Hour
+            </Heading2>
+            <Heading2>
+            5 - 7PM
+            </Heading2>
+            <br/>
+          <Heading2>
+            Drinks Menu
+          </Heading2>
+          <Heading2>
+            Tapas Menu
+          </Heading2>
+        </EventContents>
+        <EventFooterWrapper>
+          <Heading2 >VIEW DETAILS</Heading2>
+          <LongsongIcon />
+        </EventFooterWrapper>
+      </EventInfoWrapper>
+
+    </EventDetailsWrapper>
+
+  )
+}
+
+export default EventDetailsModal
