0cc6030df8aea9b33960df01419408d5d458ee85
marekreid <marekreid@gmail.com>
2021-10-08 06:49:07 +0200

    updated menu/close animation on mob menu for navigation

--- src/components/MenuContainer/MenuSlideOutContainer/SlideOutSideBar.js
+++ src/components/MenuContainer/MenuSlideOutContainer/SlideOutSideBar.js
@@ -5,16 +5,38 @@ import MenuX from "../Icons/MenuX"
 import { DesktopMenuIconWrapper, MobileMenuIconWrapper } from "../MenuContainer.css"
 import { SlideOutSideBarWrapper } from "./MenuSlideOutContianer.css"
 
-const SlideOutSideBar = ({ setMenuOpen, menuOpen, setActivePage}) => {
+const SlideOutSideBar = ({ setMenuOpen, menuOpen, activePage, setActivePage }) => {
   return (
     <SlideOutSideBarWrapper menuOpen={menuOpen}>
-       <DesktopMenuIconWrapper> <MenuX menuOpen={menuOpen} setMenuOpen={setMenuOpen} setActivePage={setActivePage} /></DesktopMenuIconWrapper>
-       <MobileMenuIconWrapper>
-        <MenuText
-         onClick={() => setMenuOpen(false)}
-        >CLOSE</MenuText>
-        <LongsongSubMenuIcon onClick={() => setMenuOpen(true)} setActivePage={setActivePage}/>
-    </MobileMenuIconWrapper>
+      <DesktopMenuIconWrapper> <MenuX menuOpen={menuOpen} setMenuOpen={setMenuOpen} setActivePage={setActivePage} /></DesktopMenuIconWrapper>
+      <MobileMenuIconWrapper>
+
+
+        {activePage > 1 ? 
+          (
+            <MenuText
+              onClick={() => setActivePage(1)}
+            >
+
+              MENU
+
+            </MenuText>
+          ) :
+          (
+            <MenuText
+              onClick={() => setMenuOpen(false)}
+            >
+
+              CLOSE
+
+            </MenuText>
+          )
+
+        }
+
+
+        <LongsongSubMenuIcon onClick={() => setMenuOpen(true)} setActivePage={setActivePage} />
+      </MobileMenuIconWrapper>
     </SlideOutSideBarWrapper>
   )
 }
