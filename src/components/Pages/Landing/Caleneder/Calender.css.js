ea84804b9de9554409039a71e5eee6e067963df9
bigluckypal010203 <bigluckypal010203@gmail.com>
2021-10-27 07:22:41 +0900

    calendar styling fix

--- src/components/Pages/Landing/Caleneder/Calender.css.js
+++ src/components/Pages/Landing/Caleneder/Calender.css.js
@@ -82,9 +82,9 @@ export const DaysWrapper = styled.div`
 `
 export const DayContainer = styled.div`
   border-top: ${props =>
-    props.month === props.currentMonth + props.month && props.day < props.date
+    (props.month === props.currentMonth + props.month && props.day < props.date)
       ? ".5px solid #6A6A6A"
-      : props.dayOfWeek == "Sunday" || props.dayOfWeek == "Tuesday"
+      : props.dayOfWeek == "Sunday" || props.dayOfWeek == "Tuesday" || props.dayOfWeek == "Monday"
       ? ".5px solid #6A6A6A"
       : (props.month === props.currentMonth + props.month &&
           props.i === props.date - 1) ||
@@ -96,14 +96,28 @@ export const DayContainer = styled.div`
         (props.month === props.currentMonth + props.month &&
           props.i === props.date + 7)
       ? ".5px solid #314638"
+      : (props.month === props.currentMonth + props.month &&
+          props.i === props.date - 2) ||
+        (props.month === props.currentMonth + props.month &&
+          props.i === props.date + 5)
+      ? ".5px solid #314638"
       : ".5px solid white"};
 
   border-bottom: ${props =>
-    props.dayOfWeek == "Sunday" || props.dayOfWeek == "Tuesday" && props.i > 22
+    (props.dayOfWeek == "Sunday" || props.dayOfWeek == "Tuesday" || props.dayOfWeek == "Monday") && (props.i > 22)
       ? "1px #6A6A6A solid"
+      : (props.month === props.currentMonth + props.month &&
+          props.i === props.date - 1) && (props.i > 22)
+      ? ".5px solid #457E5C"
+      : (props.month === props.currentMonth + props.month &&
+          props.i === props.date) && (props.i > 22)
+      ? ".5px solid #314638"
+      : (props.month === props.currentMonth + props.month &&
+          props.i === props.date - 2) && (props.i > 22)
+      ? ".5px solid #314638"
       : props.i > 22
       ? "1px solid white"
-      : props.dayOfWeek == "Sunday" || props.dayOfWeek == "Tuesday" && props.i > 27
+      : props.dayOfWeek == "Sunday" || props.dayOfWeek == "Tuesday" || props.dayOfWeek == "Monday" && props.i > 27
       ? "none"
       : "none"};
   //#457E5C
@@ -121,6 +135,8 @@ export const DayContainer = styled.div`
   }
   animation: ${fadeIn} 500ms ease-in-out;
 `
+
+
 export const EventDayContainer = styled(DayContainer)`
   border: 1px solid white;
 `
@@ -149,7 +165,7 @@ export const EventsDateDisplayContainer = styled(DateDisplayContainer)`
       : props.month === props.currentMonth + props.month &&
         props.i === props.date
       ? "#314638"
-      : props.dayOfWeek == "Sunday"
+      : props.dayOfWeek == "Sunday" || props.dayOfWeek == "Tuesday" || props.dayOfWeek == "Monday"
       ? ""
       : ""};
   border: 0.5px solid white;
