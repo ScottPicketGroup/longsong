0dbc8ba993cef3bc7461acf4beecfe9a61f7ff93
marekreid <marekreid@gmail.com>
2021-10-27 08:20:24 +0100

    changed url slug on gatbsy node

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,29 @@ import Seo from "../components/seo"
 import Hero from "../components/Pages/Landing/Hero/Hero"
 import Calender from "../components/Pages/Landing/Caleneder/Calender"
 
+//import components you have made for those sections
+
 const IndexPage = () => {
   const [calenderRef, setCalenderRef] = useState(null);
   const excuteScroll = () => calenderRef.current.scrollIntoView({behavior: "smooth", inline: "nearest"});
+
+  //components state
+
+   use effect 
+  let modulesToPush = []
+  map landingPageModules => modulesToPush.push({name: arrayname.firstlettertocapital, props: props})
+  setmodules (modulestopush)
+
+
+
 
   return (
     <Layout fd="column">
       <Seo title="Home" />
       <Hero excuteScroll={excuteScroll} />
-      <Calender setCalenderRef={setCalenderRef} />
+      {/* <Calender setCalenderRef={setCalenderRef} /> */}
+
+  {/* modules.map((module, i) => <BlankComponnet component={component.name} props={props}) */}
     </Layout>
   )
 }
