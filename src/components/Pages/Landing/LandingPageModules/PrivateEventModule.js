c0720350905e1017eb7cc27ace66ddd6d4ec3388
marekreid <marekreid@gmail.com>
2021-10-28 13:22:33 +0100

    update home page elements

--- src/components/Pages/Landing/LandingPageModules/PrivateEventModule.js
+++ src/components/Pages/Landing/LandingPageModules/PrivateEventModule.js
@@ -42,7 +42,7 @@ const PrivateEventModule = () => {
   
   `)
 
-const {gallery, intro, title } = data.allContentfulLongsongLandingPagePrivateDiningSection.edges[0].node
+const {gallery, intro, title } = data.allContentfulLongsongLandingPagePrivateDiningSection.edges[1].node
 
 
   return (
