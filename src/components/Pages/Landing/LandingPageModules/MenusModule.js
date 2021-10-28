2ad41f263d182af9a9a9d4f335487ea55901dbeb
marekreid <marekreid@gmail.com>
2021-10-28 09:20:13 +0100

    updated landing page modules with new queiries

--- src/components/Pages/Landing/LandingPageModules/MenusModule.js
+++ src/components/Pages/Landing/LandingPageModules/MenusModule.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 import Slider from "../../../ImageSlider"
 import {
@@ -13,21 +13,64 @@ import {
 import { BreakLine } from "../../../MenuContainer/MenuSlideOutContainer/SlideOutMenuNavigation/SlideOutMenuNavigation.css"
 import { Heading1, BC1 } from "../../../global-styles/typography.css"
 
-const MenusModule = ({ data }) => {
+const MenusModule = () => {
+  const data = useStaticQuery(graphql`
+  query menusModuleData {
+    allContentfulLongsongLandingPageMenuSection {
+      edges {
+        node {
+          id
+          foodMenuImage {
+            gatsbyImageData(aspectRatio: 1)
+          }
+          drinksMenuImage {
+            gatsbyImageData(aspectRatio: 1)
+          }
+          foodMenuHeading
+          drinksMenuHeading
+          foodMenuIntro
+          drinksMenuIntro
+          drinksMenuPdf {
+            file {
+              url
+            }
+          }
+          foodMenuPdf {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+  `)
+
+  const {
+    foodMenuHeading,
+          drinksMenuHeading,
+          foodMenuIntro,
+          drinksMenuIntro,
+          drinksMenuImage,
+          drinksMenuPdf,
+          foodMenuPdf,
+          foodMenuImage
+
+   } = data.allContentfulLongsongLandingPageMenuSection.edges[0].node  
 
   return (
     <LandingPageModuleContainer>
       <MenusModuleWrapper>
         <MenusModuleHalfWrapper>
           <ImageWrapper>
-            <Slider imageData={data.foodMenuImage}></Slider>
+            <Slider imageData={foodMenuImage}></Slider>
           </ImageWrapper>
           <EventContentContainer style={{ marginTop: "3rem" }}>
             <TextContainer full>
               <BreakLine none />
-              <Heading1 marginBottom="md">{data.foodMenuHeading}</Heading1>
-              <BC1 marginBottom="md">{data.foodMenuIntro}</BC1>
-              <Link to={data.foodMenuPdf.file.url}>
+              <Heading1 marginBottom="md">{foodMenuHeading}</Heading1>
+              <BC1 marginBottom="md">{foodMenuIntro}</BC1>
+              <Link to={foodMenuPdf.file.url}>
                 <BC1 style={{ color: "#457E5C" }}>View menu</BC1>
               </Link>
             </TextContainer>
@@ -36,14 +79,14 @@ const MenusModule = ({ data }) => {
 
         <MenusModuleHalfWrapper>
           <ImageWrapper>
-            <Slider imageData={data.drinksMenuImage}></Slider>
+            <Slider imageData={drinksMenuImage}></Slider>
           </ImageWrapper>
           <EventContentContainer style={{ marginTop: "3rem" }}>
             <TextContainer full>
               <BreakLine none />
-              <Heading1 marginBottom="md">{data.drinksMenuHeading}</Heading1>
-              <BC1 marginBottom="md">{data.drinksMenuIntro}</BC1>
-              <Link to={data.drinksMenuPdf.file.url}>
+              <Heading1 marginBottom="md">{drinksMenuHeading}</Heading1>
+              <BC1 marginBottom="md">{drinksMenuIntro}</BC1>
+              <Link to={drinksMenuPdf.file.url}>
                 <BC1 style={{ color: "#457E5C" }}>View drinks list</BC1>
               </Link>
             </TextContainer>
