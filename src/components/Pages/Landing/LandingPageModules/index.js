832fd10d9ccdd7208ffe12a0997d8961375a3329
marekreid <marekreid@gmail.com>
2021-10-28 07:03:06 +0100

    updated footer requests

--- src/components/Pages/Landing/LandingPageModules/index.js
+++ src/components/Pages/Landing/LandingPageModules/index.js
@@ -8,9 +8,9 @@ const LandingEventsContainer = ({ data }) => {
   console.log(data)
   return (
     <>
-{/*       
+      
       <MenusModule data={data[0]} />
-      <EventsModule data={data[2].landingPageEventsList} />
+      {/* <EventsModule data={data[2].landingPageEventsList} />
       <PrivateEventModule data={data[1]} /> */}
     </>
   )
