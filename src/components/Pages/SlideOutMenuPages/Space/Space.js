d19faa8b8d999f778ce3a66e3e843033f362fd23
marekreid <marekreid@gmail.com>
2021-10-28 06:55:08 +0100

    updated footer requests

--- src/components/Pages/SlideOutMenuPages/Space/Space.js
+++ src/components/Pages/SlideOutMenuPages/Space/Space.js
@@ -5,42 +5,57 @@ import { SlideOutPageWrapper } from "./Space.css"
 
 const Space = ({ activePage }) => {
   const data = useStaticQuery(graphql`
-  query s {
-    allContentfulLongsongPageContent {
-      edges {
-        node {
-          id
-          barSnacksGallery {
-            gatsbyImageData
-          }
-        }
+  query stallsAndYard {
+    contentfulLongsongPageContent(id: {eq: "a93d3e6e-66c6-56f3-870e-bbd14f35a26e"}) {
+      theStallsTitle
+      theStallsMedia {
+        gatsbyImageData
+        title
+      }
+      theStallsDescription {
+        raw
+      }
+      theYardTitle
+      theYardMedia {
+        gatsbyImageData
+        title
+      }
+      theYardContent {
+        raw
+      }
+      featuredPerformerTitle
+      featuredPerformerDescription {
+        raw
+      }
+      barSnacksGallery {
+        gatsbyImageData
       }
     }
   }
   
   `)
 
-  // const newData = {
-  //   firstSection: {
-  //     title: data.contentfulLongsongPageContent.theStallsTitle,
-  //     content: data.contentfulLongsongPageContent.theStallsDescription,
-  //     imageData: data.contentfulLongsongPageContent.theStallsMedia,
-  //   },
-  //   secondSection: {
-  //     title: data.contentfulLongsongPageContent.theYardTitle,
-  //     content: data.contentfulLongsongPageContent.theYardContent,
-  //     imageData: data.contentfulLongsongPageContent.theYardMedia
-  //   },
-  //   thirdSection: {
-  //     title: data.contentfulLongsongPageContent.featuredPerformerTitle,
-  //     content: data.contentfulLongsongPageContent.featuredPerformerDescription,
-  //     imageData: data.contentfulLongsongPageContent.featuredPerformerMedia
-  //   }
-  // }
+  const newData = {
+    firstSection: {
+      title: data.contentfulLongsongPageContent.theStallsTitle,
+      content: data.contentfulLongsongPageContent.theStallsDescription,
+      imageData: data.contentfulLongsongPageContent.theStallsMedia,
+    },
+    secondSection: {
+      title: data.contentfulLongsongPageContent.theYardTitle,
+      content: data.contentfulLongsongPageContent.theYardContent,
+      imageData: data.contentfulLongsongPageContent.theYardMedia
+    },
+    thirdSection: {
+      title: data.contentfulLongsongPageContent.featuredPerformerTitle,
+      content: data.contentfulLongsongPageContent.featuredPerformerDescription,
+      imageData: data.contentfulLongsongPageContent.barSnacksGallery
+    }
+  }
 
   return (
     <SlideOutPageWrapper activePage={activePage} page={2}>
-      {/* <StallsAndYard data={newData} /> */}
+     <StallsAndYard data={newData} /> 
     </SlideOutPageWrapper>
   )
 }
