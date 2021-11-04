14155bcd984b55c20c8f6b693f60efd7bd9af87b
marekreid <marekreid@gmail.com>
2021-11-04 16:45:50 +0000

    removed modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,78 +24,78 @@ const IndexPage = ({ data }) => {
 
 export default IndexPage
 
-// export const landingPageData = graphql`
-// query landing {
-//   allContentfulLongsongLandingPageContent {
-//     edges {
-//       node {
-//         id
-//         landingPageSections {
-//           ... on ContentfulLandingPageEventsModule {
-//             id
-//             landingPageEventsList {
-//               id
-//               eventDate(formatString: "DD.MM")
-//               eventName
-//               slug
-//               eventDescription {
-//                 raw
-//               }
-//               eventMedia {
-//                 gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
-//               }
-//             }
-//           }
-//           ... on ContentfulLongsongLandingPagePrivateDiningSection {
-//             id
-//             gallery {
-//               gatsbyImageData
-//               title
-//             }
-//             intro {
-//               raw
-//             }
-//             title
-//           }
-//           ... on ContentfulLongsongLandingPageMenuSection {
-//             id
-//             foodMenuImage {
-//               gatsbyImageData(aspectRatio: 1)
-//             }
-//             drinksMenuImage {
-//               gatsbyImageData(aspectRatio: 1)
-//             }
-//             foodMenuHeading
-//             drinksMenuHeading
-//             foodMenuIntro
-//             drinksMenuIntro
-//             drinksMenuPdf {
-//               file {
-//                 url
-//               }
-//             }
-//             foodMenuPdf {
-//               file {
-//                 url
-//               }
-//             }
-//           }
-//         }
-//         textOverHeroImage {
-//             raw
-//           }
-//         heroImage {
+export const landingPageData = graphql`
+query landing {
+  allContentfulLongsongLandingPageContent {
+    edges {
+      node {
+        id
+        landingPageSections {
+          ... on ContentfulLandingPageEventsModule {
+            id
+            landingPageEventsList {
+              id
+              eventDate(formatString: "DD.MM")
+              eventName
+              slug
+              eventDescription {
+                raw
+              }
+              eventMedia {
+                gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
+              }
+            }
+          }
+          ... on ContentfulLongsongLandingPagePrivateDiningSection {
+            id
+            gallery {
+              gatsbyImageData
+              title
+            }
+            intro {
+              raw
+            }
+            title
+          }
+          ... on ContentfulLongsongLandingPageMenuSection {
+            id
+            foodMenuImage {
+              gatsbyImageData(aspectRatio: 1)
+            }
+            drinksMenuImage {
+              gatsbyImageData(aspectRatio: 1)
+            }
+            foodMenuHeading
+            drinksMenuHeading
+            foodMenuIntro
+            drinksMenuIntro
+            drinksMenuPdf {
+              file {
+                url
+              }
+            }
+            foodMenuPdf {
+              file {
+                url
+              }
+            }
+          }
+        }
+        textOverHeroImage {
+            raw
+          }
+        heroImage {
 
-//         file {
-//               url
-//               details {
-//                 size
-//               }
-//             }
-//             gatsbyImageData(placeholder: BLURRED)}
-//       }
-//     }
-//   }
-// }
+        file {
+              url
+              details {
+                size
+              }
+            }
+            gatsbyImageData(placeholder: BLURRED)}
+      }
+    }
+  }
+}
 
-// `
+`
