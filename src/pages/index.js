4a5fc9c391fea01543f6d6d08cf6d5dcd4d89ffc
marekreid <marekreid@gmail.com>
2021-11-04 16:42:20 +0000

    removed modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,104 +7,95 @@ import HeroBackground from "../components/Pages/Landing/Hero/HeroBackgound"
 import LandingPageModules from "../components/Pages/Landing/LandingPageModules"
 
 const IndexPage = ({ data }) => {
-  const { heroImage, textOverHeroImage, landingPageSections } =
-    data.allContentfulLongsongLandingPageContent.edges[0].node
+  // const { heroImage, textOverHeroImage, landingPageSections } =
+  //   data.allContentfulLongsongLandingPageContent.edges[0].node
 
-  //components state
 
-  //  use effect 
-  // let modulesToPush = []
-  // map landingPageModules => modulesToPush.push({name: arrayname.firstlettertocapital, props: props})
-  // setmodules (modulestopush)
 
 
 
 
   return (
-    <Layout fd="column" heroImage={heroImage}>
-      <Seo title="Home" />
-      {/* <HeroBackground 
-        
-      />
-      <LandingPageModules data={landingPageSections} /> */}
-    </Layout>
+ <>
+ hi 
+ </>
   )
 }
 
 export default IndexPage
 
-export const landingPageData = graphql`
-query landing {
-  allContentfulLongsongLandingPageContent {
-    edges {
-      node {
-        id
-        landingPageSections {
-          ... on ContentfulLandingPageEventsModule {
-            id
-            landingPageEventsList {
-              id
-              eventDate(formatString: "DD.MM")
-              eventName
-              slug
-              eventDescription {
-                raw
-              }
-              eventMedia {
-                gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
-              }
-            }
-          }
-          ... on ContentfulLongsongLandingPagePrivateDiningSection {
-            id
-            gallery {
-              gatsbyImageData
-              title
-            }
-            intro {
-              raw
-            }
-            title
-          }
-          ... on ContentfulLongsongLandingPageMenuSection {
-            id
-            foodMenuImage {
-              gatsbyImageData(aspectRatio: 1)
-            }
-            drinksMenuImage {
-              gatsbyImageData(aspectRatio: 1)
-            }
-            foodMenuHeading
-            drinksMenuHeading
-            foodMenuIntro
-            drinksMenuIntro
-            drinksMenuPdf {
-              file {
-                url
-              }
-            }
-            foodMenuPdf {
-              file {
-                url
-              }
-            }
-          }
-        }
-        textOverHeroImage {
-            raw
-          }
-        heroImage {
+// export const landingPageData = graphql`
+// query landing {
+//   allContentfulLongsongLandingPageContent {
+//     edges {
+//       node {
+//         id
+//         landingPageSections {
+//           ... on ContentfulLandingPageEventsModule {
+//             id
+//             landingPageEventsList {
+//               id
+//               eventDate(formatString: "DD.MM")
+//               eventName
+//               slug
+//               eventDescription {
+//                 raw
+//               }
+//               eventMedia {
+//                 gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
+//               }
+//             }
+//           }
+//           ... on ContentfulLongsongLandingPagePrivateDiningSection {
+//             id
+//             gallery {
+//               gatsbyImageData
+//               title
+//             }
+//             intro {
+//               raw
+//             }
+//             title
+//           }
+//           ... on ContentfulLongsongLandingPageMenuSection {
+//             id
+//             foodMenuImage {
+//               gatsbyImageData(aspectRatio: 1)
+//             }
+//             drinksMenuImage {
+//               gatsbyImageData(aspectRatio: 1)
+//             }
+//             foodMenuHeading
+//             drinksMenuHeading
+//             foodMenuIntro
+//             drinksMenuIntro
+//             drinksMenuPdf {
+//               file {
+//                 url
+//               }
+//             }
+//             foodMenuPdf {
+//               file {
+//                 url
+//               }
+//             }
+//           }
+//         }
+//         textOverHeroImage {
+//             raw
+//           }
+//         heroImage {
 
-        file {
-              url
-              details {
-                size
-              }
-            }
-            gatsbyImageData(placeholder: BLURRED)}
-      }
-    }
-  }
-}
+//         file {
+//               url
+//               details {
+//                 size
+//               }
+//             }
+//             gatsbyImageData(placeholder: BLURRED)}
+//       }
+//     }
+//   }
+// }
 
-`
+// `
