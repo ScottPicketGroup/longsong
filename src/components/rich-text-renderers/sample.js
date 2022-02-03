d978c2a976e895d9522cb75e193dd34b2f6827e4
marekreid <marekreid@gmail.com>
2022-02-03 12:20:45 +0100

    added clo to bar page

--- src/components/rich-text-renderers/sample.js
+++ src/components/rich-text-renderers/sample.js
@@ -10,7 +10,7 @@ import {
 import { Button } from "../global-styles/GlobalStyles.css"
 
 const Renderer = ({ node }) => {
-  console.log(node)
+  
   const Bold = ({ children }) => (
     <span style={{ fontFamily: `bold` }}>{children}</span>
   )
