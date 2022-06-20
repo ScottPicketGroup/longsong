655567babc657861428a10276e3aa76531e6b76d
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-06-20 18:25:59 +0200

    updated intro logo

--- src/components/Pages/Landing/Hero/LongsongVideo/LongsongVideo.js
+++ src/components/Pages/Landing/Hero/LongsongVideo/LongsongVideo.js
@@ -11,7 +11,7 @@ const LongsongVideo = () => {
         }, 1800);
         const timerOut = setTimeout(() => {
             setShow("heading");
-          }, 7000);
+          }, 6500);
         return () => clearTimeout(timer);
       }, []);
    
