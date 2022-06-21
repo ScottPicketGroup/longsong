1222eab850be31b4647895a08a53b2aea23aa3e0
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-06-21 10:31:28 +0200

    changed form

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,26 +1,29 @@
-
+import * as React from "react"
 import GlobalStyles from "./src/components/global-styles/GlobalStyles.css"
 import { Typography } from "./src/components/global-styles/typography.css"
 import { ChangeMonthProvider } from "./src/components/hooks/ChangeMonth"
 import { ActivePageProvider } from "./src/components/hooks/ActivePage"
 
 import React, {useState, useEffect} from "react"
-import { Helmet } from "react-helmet"
 
 const App = ({ element }) => {
   useEffect(() => {
  // set up and use external package as needed
-console.log(window.ftenquireinit(665, "longrain"))
+ window.externalLibrary.method()
    }, [])
   return (
     <>
       <Helmet>
         
-         
-      <script
+            <script
               language="javascript"
               src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"
             ></script>
+            <script language="javascript">
+            {window.enquirecustom.ftenquireinit(665, "longrain")}
+            </script>
+            <div id="ftenquire665"></div>
+        
         
       </Helmet>
       {/* <GlobalStyles />
