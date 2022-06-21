91456fbf21c785d5ba4e7387f5bbfb1eaa5ceeb5
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-06-21 09:19:36 +0200

    changed form

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
+import React from "react"
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const test = () => {
+  const [form, setForm] = React.useState(false)
+  React.useEffect(() => {
+    setForm(true)
+  }, [])
   return (
-    <Layout fd="column" >
-    <Seo title="Home" />
-     test
-     <Helmet>
-
-     <script language="javascript" src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"></script><script language="javascript">setTimeout(function(){ ftenquireinit(665,'longrain')}, 2000)</script><div id="ftenquire665"></div>
-     </Helmet>
-  </Layout>
+    <Layout fd="column">
+      <Seo title="Home" />
+      test
+      <Helmet>
+        {form ? (
+          <>
+            <script
+              language="javascript"
+              src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"
+            ></script>
+            <script language="javascript">
+              setTimeout(function(){ftenquireinit(665, "longrain")}, 2000)
+            </script>
+            <div id="ftenquire665"></div>
+          </>
+        ) : null}
+      </Helmet>
+    </Layout>
   )
 }
 
