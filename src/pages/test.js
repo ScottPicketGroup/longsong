e5b2d8c09376dd0826412aece21a112fed643eca
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-06-21 06:23:11 +0200

    added function tracker on /test

---
+++ src/pages/test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const test = () => {
+  return (
+    <Layout fd="column" >
+    <Seo title="Home" />
+     test
+     <Helmet>
+
+     <script language="javascript" src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"></script><script language="javascript">setTimeout(function(){ ftenquireinit(665,'longrain')}, 3000)</script><div id="ftenquire665"></div>
+     </Helmet>
+  </Layout>
+  )
+}
+
+export default test
