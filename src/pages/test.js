7f5bb6f9a6b0819fa7ce974843eb8a607c85e32c
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-06-24 09:02:59 +0200

    fingers crosed

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,36 +1,39 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const Test = () => {
-//   const [form, setForm] = useState(false)
-//  useEffect(() => {
-//    setTimeout(() => {
-//     setForm(true)
-//    }, 100000);
-//   }, [])
-//   console.log(form)
-// useEffect(() => {
-//     console.log(window.ftenquireinit(665, "longrain"))
-    
-// }, []);
-setTimeout(() => {
-  window.ftenquireinit(665,"longrain")
-}, 3000);
-console.log('window', window)
+  //   const [form, setForm] = useState(false)
+  //  useEffect(() => {
+  //    setTimeout(() => {
+  //     setForm(true)
+  //    }, 100000);
+  //   }, [])
+  //   console.log(form)
+  // useEffect(() => {
+  //     console.log(window.ftenquireinit(665, "longrain"))
+
+  // }, []);
+  // setTimeout(() => {
+  //   window.ftenquireinit(665, "longrain")
+  // }, 3000)
+  console.log("window", window)
   return (
     <Layout fd="column">
       <Seo title="Home" />
       <Helmet>
-        
-      <script language="javascript" src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"></script><script language="javascript">{ setTimeout(() => {
-  window.ftenquireinit(665,"longrain")
-}, 3000)}</script><div id="ftenquire665"></div>
-       
-    
-    
-  </Helmet>
+        <script
+          language="javascript"
+          src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"
+        ></script>
+        <script language="javascript">
+          {setTimeout(() => {
+            window.ftenquireinit(665, "longrain")
+          }, 3000)}
+        </script>
+        <div id="ftenquire665"></div>
+      </Helmet>
       {/* <Helmet>
         {form ? (
           <>
@@ -45,8 +48,8 @@ console.log('window', window)
           </>
         ) : null}
       </Helmet> */}
-      
-       {/* <div id="ftenquire665"></div> */}
+
+      {/* <div id="ftenquire665"></div> */}
     </Layout>
   )
 }
