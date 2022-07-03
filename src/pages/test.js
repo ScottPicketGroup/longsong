d3b1cc7de95d82688e784673f8effd4b586b37a6
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-07-03 11:17:13 +0200

    form testing

--- src/pages/test.js
+++ src/pages/test.js
@@ -25,4 +25,54 @@ background-color: inherit;
   .ftpage-container {
     background-color: blue;
   }
+
+.select2-selection__rendered {  ;color:white  ; background-color:#FFFFFF}
+
+/* Around the search field */
+.select2-search { ;color:white  ; background-color:#FFFFFF }
+
+/* Search field */
+.select2-search input { ;color:white  ; background-color:#FFFFFF }
+
+/* Each result */
+.select2-results { ;color:white  ; background-color:#FFFFFF }
+
+/* Higlighted (hover) result */
+.select2-results__option--highlighted { ;color:white   }
+
+/* Selected option */
+.select2-results__option[aria-selected=true] { ;color:white   }
+
+.select2-container{  border:1px solid #dddddd; width: 100% !important ; }
+
+.select2-selection--single:not([class*="bg-"]) { ; background-color:#FFFFFF  }
+
+.select2-selection--multiple:not([class*="bg-"]){ ; background-color:#FFFFFF }
+
+.select2-selection__arrow{ ; background-color:#FFFFFF }
+.select2-selection__placeholder {
+  color: white   !important;
+}
+.select2-search__field {
+  color: white   !important;
+}
+.select2-selection__arrow::before {
+    content: "";
+    display:none;
+}
+
+.form-control-uniform {
+    color: white   !important; font-weight: bold !important;
+}
+.ftform-control::-webkit-input-placeholder { color: white  ; }  /* WebKit, Blink, Edge */
+.ftform-control:-moz-placeholder { color: white  ; }  /* Mozilla Firefox 4 to 18 */
+.ftform-control::-moz-placeholder { color: white  ; }  /* Mozilla Firefox 19+ */
+.ftform-control:-ms-input-placeholder { color: white  ; }  /* Internet Explorer 10-11 */
+.ftform-control::-ms-input-placeholder { color: white  ; }  /* Microsoft Edge */
+	.checker span { color:#dddddd ; border: 2px solid #dddddd; }
+    .picker__header  {
+    ;color:white      }
+    .picker__table {
+        ;color:white      }
+
 `;
