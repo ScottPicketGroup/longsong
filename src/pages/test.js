ab81424ad67d8d66857592d27d36feb1b955c6c6
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-07-03 10:52:30 +0200

    form testing

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,56 +4,30 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const Test = () => {
-  //   const [form, setForm] = useState(false)
-  //  useEffect(() => {
-  //    setTimeout(() => {
-  //     setForm(true)
-  //    }, 100000);
-  //   }, [])
-  //   console.log(form)
-  // useEffect(() => {
-  //     console.log(window.ftenquireinit(665, "longrain"))
 
-  // }, []);
-  // setTimeout(() => {
-  //   window.ftenquireinit(665, "longrain")
-  // }, 3000)
+
   
-  console.log("form info:", window.ftenquireinit(665, "longrain"))
   return (
     <Layout fd="column">
       <Seo title="Home" />
-      {/* <Helmet>
-        <script
-          language="javascript"
-          src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"
-        ></script>
-        <script language="javascript">
-          {setTimeout(() => {
-            window.ftenquireinit(665, "longrain")
-          }, 3000)}
-        </script>
-       
-      </Helmet> */}
-      <div id="ftenquire665"></div>
-      {/* <Helmet>
-        {form ? (
-          <>
-            <script
-              language="javascript"
-              src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"
-            ></script>
-            <script language="javascript">
-            {ftenquireinit(665, "longrain")}
-            </script>
-            <div id="ftenquire665"></div>
-          </>
-        ) : null}
-      </Helmet> */}
-
-      {/* <div id="ftenquire665"></div> */}
+   
+      <div id="ftenquire665" style={styles.section}></div>
+    
     </Layout>
   )
 }
 
 export default Test
+const styles = {
+  section: {
+    fontSize: "18px",
+    color: "#292b2c",
+    backgroundColor: "#fff",
+    padding: "0 20px"
+  },
+  wrapper: {
+    textAlign: "center",
+    margin: "0 auto",
+    marginTop: "50px"
+  }
+}
