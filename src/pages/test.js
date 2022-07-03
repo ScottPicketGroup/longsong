6b78dc0a0043343cf6e8b2bc8dd83e2c099f4b82
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-07-03 11:24:03 +0200

    form testing

--- src/pages/test.js
+++ src/pages/test.js
@@ -17,7 +17,7 @@ const Test = () => {
 export default Test
 
 export const FunctionTrackerForm = styled.div`
-  background-color: blue;
+  background-color: blue !important;
   .ftpage-container {
     background-color: blue;
   }
