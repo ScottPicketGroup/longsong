f190546dcc620206f242cf80eb61697dab19e9a1
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-07-03 10:35:20 +0200

    form testing

--- src/components/Pages/SlideOutMenuPages/PrivateDining/ContactForm/FunctionTrackerForm.js
+++ src/components/Pages/SlideOutMenuPages/PrivateDining/ContactForm/FunctionTrackerForm.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
+import { TextContainer } from "../../Bar/Space.css"
 const FunctionTrackerForm = () => {
   return (
-    <FunctionTrackerFormContainer id="ftenquire665"></FunctionTrackerFormContainer>
+ 
+      <FunctionTrackerFormContainer id="ftenquire665"></FunctionTrackerFormContainer>
+
   )
 }
 
 export default FunctionTrackerForm
 
-export const FunctionTrackerFormContainer = styled.div`
-
-  
-`;
+export const FunctionTrackerFormContainer = styled.div``
