5217fc95997933159726b985ccc66cfefc9162d2
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-07-03 10:30:30 +0200

    moved div into main body

---
+++ src/components/Pages/SlideOutMenuPages/PrivateDining/ContactForm/FunctionTrackerForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FunctionTrackerForm = () => {
+  return (
+    <div id="ftenquire665"></div>
+  )
+}
+
+export default FunctionTrackerForm
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,57 +1,17 @@
-import React, { useState, useEffect } from "react"
-import { Helmet } from "react-helmet"
+import React, {} from "react"
+
 import Layout from "../components/layout"
+import FunctionTrackerForm from "../components/Pages/SlideOutMenuPages/PrivateDining/ContactForm/FunctionTrackerForm"
 import Seo from "../components/seo"
 
 const Test = () => {
-  //   const [form, setForm] = useState(false)
-  //  useEffect(() => {
-  //    setTimeout(() => {
-  //     setForm(true)
-  //    }, 100000);
-  //   }, [])
-  //   console.log(form)
-  // useEffect(() => {
-  //     console.log(window.ftenquireinit(665, "longrain"))
 
-  // }, []);
-  // setTimeout(() => {
-  //   window.ftenquireinit(665, "longrain")
-  // }, 3000)
-  
-  console.log("form info:", window.ftenquireinit(665, "longrain"))
   return (
     <Layout fd="column">
-      <Seo title="Home" />
-      {/* <Helmet>
-        <script
-          language="javascript"
-          src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"
-        ></script>
-        <script language="javascript">
-          {setTimeout(() => {
-            window.ftenquireinit(665, "longrain")
-          }, 3000)}
-        </script>
-       
-      </Helmet> */}
-      <div id="ftenquire665"></div>
-      {/* <Helmet>
-        {form ? (
-          <>
-            <script
-              language="javascript"
-              src="https://longrain.functiontracker.com/public/v4/enquirecustom.js"
-            ></script>
-            <script language="javascript">
-            {ftenquireinit(665, "longrain")}
-            </script>
-            <div id="ftenquire665"></div>
-          </>
-        ) : null}
-      </Helmet> */}
-
-      {/* <div id="ftenquire665"></div> */}
+      <Seo title="Spaces" />
+     <FunctionTrackerForm/>
+      
+     
     </Layout>
   )
 }
