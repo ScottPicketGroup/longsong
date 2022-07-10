f28b750eeff4d686d55aa62c7587e9dcd911e9d5
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-07-10 12:31:50 +0200

    update function tracker selector

--- src/components/Pages/SlideOutMenuPages/PrivateDining/ContactForm/FunctionTrackerForm.js
+++ src/components/Pages/SlideOutMenuPages/PrivateDining/ContactForm/FunctionTrackerForm.js
@@ -249,4 +249,7 @@ export const FunctionTrackerFormContainer = styled.div`
   border: 1px solid #F0E9D5;
   transition: all ease-in-out 300ms;
 }
+#uniform-subcribemailinglistfield > span {
+  background: blue;
+}
 `
