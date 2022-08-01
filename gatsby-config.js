0d15959c16593fd2e6236ab976f9b71596a9f0be
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-08-01 10:10:52 +0200

    amended gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ require("dotenv").config({
 })
 
 module.exports = {
+  
   siteMetadata: {
     title: `Longsong Bar`,
     description: `Welcome to where all the fun is had`,
@@ -10,6 +11,31 @@ module.exports = {
     siteUrl: `https://www.longsong.com.au`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-V2N3P8NKJN", // Google Analytics / GA
+        //  "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+        //  "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+      
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          // respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+         // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+         // origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
+      },
+    },
     {
       resolve: `gatsby-source-contentful`,
       options: {
@@ -78,34 +104,6 @@ module.exports = {
     //     enableWebVitalsTracking: true,
     //   },
     // },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-V2N3P8NKJN", // Google Analytics / GA
-        //  "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-        //  "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-         // optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "YOUR_SELF_HOSTED_ORIGIN",
-        },
-      },
-    },
+    
   ],
 }
