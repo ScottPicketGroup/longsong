a18e38ee7c13f55148282476b90db79ce1aea1dd
Lmarcho-NeoSolax <your_email@example.com>
2024-06-06 10:46:03 +0530

    staging changes remove

--- src/components/Pages/Landing/LongrainLink/LongrainLink.js
+++ src/components/Pages/Landing/LongrainLink/LongrainLink.js
@@ -11,7 +11,7 @@ const LongrainLink = ({ heroImage }) => {
           target="_blank"
           rel="noreferrer"
         >
-          LONGRAIN STAGING
+          LONGRAIN
         </a>
         <Underline /> 
       </BC4>
